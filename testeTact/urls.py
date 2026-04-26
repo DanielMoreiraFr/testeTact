2cb3fc3c628b769eb56f95c7349f98c8b9abfb69
Daniel Moreira <moreiradefranca.daniel@gmail.com>
2026-04-25 23:56:45 -0300

    home + botao temp para data

--- testeTact/urls.py
+++ testeTact/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
+app_name='site'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.site_home, name='home'),
     path('data/', include('data.urls')),
 ]
